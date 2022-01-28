bae01e89fe33a2de23c32b963a098f9fb9777309
goodboy3 <zhangzhengbo@hotmail.com>
2022-01-28 10:08:52 +0800

    commit

--- plugin/cache/cache.go
+++ plugin/cache/cache.go
@@ -4,21 +4,6 @@ import (
 	"github.com/universe-30/UCache"
 )
 
-//var cache *UCache.Cache
-//var once sync.Once
-//
-//func Init(){
-//	//only run once
-//	once.Do(func() {
-//		cache = UCache.New()
-//	})
-//}
-//
-//func GetSingleInstance() *UCache.Cache {
-//	Init()
-//	return cache
-//}
-
 var cache *UCache.Cache
 
 func GetSingleInstance() *UCache.Cache {
