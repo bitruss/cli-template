8e526ddbb7cb71a111ec4297962b154a692ac007
goodboy3 <zhangzhengbo@hotmail.com>
2022-03-14 10:43:36 +0800

    commit

---
+++ tools/cache/LCache.go
@@ -0,0 +1,14 @@
+package cache
+
+import (
+	"github.com/coreservice-io/UCache"
+)
+
+func Local_Get(localCache *UCache.Cache, keystr string) (result interface{}) {
+	localvalue, ttl, localexist := localCache.Get(keystr)
+	if !CheckTtlRefresh(ttl) && localexist {
+		return localvalue
+	}
+	//need get from origin
+	return nil
+}
