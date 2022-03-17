042787e4d1dd65604cccc248e9c9bbcbdc68108d
leo <leo@leos-MacBook-Pro.local>
2022-03-17 16:29:21 +0800

    udpate

--- src/examples/dataInRedis/keyValue.go
+++ src/examples/dataInRedis/keyValue.go
@@ -6,7 +6,6 @@ import (
 	"github.com/coreservice-io/CliAppTemplate/plugin/redisClient"
 	"github.com/coreservice-io/CliAppTemplate/plugin/reference"
 	"github.com/coreservice-io/CliAppTemplate/tools/smartCache"
-	"github.com/go-redis/redis/v8"
 )
 
 //example for GormDB and tools cache
@@ -46,9 +45,6 @@ func GetPeer(tag string, forceUpdate bool) *PeerInfo {
 	// try to get from redis
 	var redis_result *PeerInfo
 	err := smartCache.Redis_Get(context.Background(), redisClient.GetInstance(), true, key, redis_result)
-	if err == redis.Nil {
-		return nil
-	}
 	if err == nil {
 		smartCache.Ref_Set(reference.GetInstance(), key, redis_result)
 		return redis_result
