6db456107a4bae0177d89b4129f1039258cd673d
leo <leo@leos-MacBook-Pro.local>
2022-05-09 11:13:36 +0800

    update

--- cmd/default_/plugin/auto_cert.go
+++ cmd/default_/plugin/auto_cert.go
@@ -10,42 +10,44 @@ import (
 
 func initAutoCert() error {
 
+	///////
 	auto_cert_crt_path, err := configuration.Config.GetString("auto_cert_crt_path", "")
 	if err != nil {
 		return errors.New("auto_cert_crt_path [string] in config err," + err.Error())
 	}
-
-	if auto_cert_crt_path != "" {
-		auto_cert_crt_path_abs, auto_cert_crt_path_abs_err := path_util.SmartExistPath(auto_cert_crt_path)
-		if auto_cert_crt_path_abs_err != nil {
-			return errors.New("auto_cert_crt_path  error," + auto_cert_crt_path_abs_err.Error())
-		}
-		auto_cert_crt_path = auto_cert_crt_path_abs
+	if auto_cert_crt_path == "" {
+		return errors.New("auto_cert_crt_path config error")
 	}
+	auto_cert_crt_path_abs, auto_cert_crt_path_abs_err := path_util.SmartExistPath(auto_cert_crt_path)
+	if auto_cert_crt_path_abs_err != nil {
+		return errors.New("auto_cert_crt_path  error," + auto_cert_crt_path_abs_err.Error())
+	}
+	///////
 
 	auto_cert_key_path, err := configuration.Config.GetString("auto_cert_key_path", "")
 	if err != nil {
 		return errors.New("auto_cert_key_path [string] in config err," + err.Error())
 	}
-
-	if auto_cert_key_path != "" {
-		auto_cert_key_path_abs, auto_cert_key_path_abs_err := path_util.SmartExistPath(auto_cert_key_path)
-		if auto_cert_key_path_abs_err != nil {
-			return errors.New("auto_cert_key_path  error," + auto_cert_key_path_abs_err.Error())
-		}
-		auto_cert_key_path = auto_cert_key_path_abs
+	if auto_cert_key_path == "" {
+		return errors.New("auto_cert_key_path config error")
 	}
-
+	auto_cert_key_path_abs, auto_cert_key_path_abs_err := path_util.SmartExistPath(auto_cert_key_path)
+	if auto_cert_key_path_abs_err != nil {
+		return errors.New("auto_cert_key_path  error," + auto_cert_key_path_abs_err.Error())
+	}
+	////////////
 	auto_cert_url, err := configuration.Config.GetString("auto_cert_url", "")
 	if err != nil {
 		return errors.New("auto_cert_url [string] in config err," + err.Error())
+	}
+	if auto_cert_url == "" {
+		return errors.New("auto_cert_url config error")
 	}
 
 	auto_cert_check_interval, err := configuration.Config.GetInt("auto_cert_check_interval", 3600)
 	if err != nil || auto_cert_check_interval <= 5 {
 		return errors.New("auto_cert_check_interval [int64] in config err or too small interval")
 	}
-
 	auto_cert_init_download, err := configuration.Config.GetBool("auto_cert_init_download", true)
 	if err != nil {
 		return errors.New("auto_cert_init_download [bool] in config err or too small interval")
@@ -53,8 +55,8 @@ func initAutoCert() error {
 
 	return auto_cert_plugin.Init(auto_cert_plugin.Config{
 		Download_url:        auto_cert_url,
-		Local_crt_path:      auto_cert_crt_path,
-		Local_key_path:      auto_cert_key_path,
+		Local_crt_path:      auto_cert_crt_path_abs,
+		Local_key_path:      auto_cert_key_path_abs,
 		Check_interval_secs: auto_cert_check_interval,
 	}, auto_cert_init_download)
 
