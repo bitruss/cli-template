cb739c222ea8c7edcc41fca30694411b505c78ad
goodboy3 <zhangzhengbo@hotmail.com>
2022-10-23 15:41:56 +0800

    commit

--- src/common/redis_pipeline/operation.go
+++ src/common/redis_pipeline/operation.go
@@ -16,24 +16,26 @@ const (
 	operation_ZRemRangeByScore = "ZRemRangeByScore"
 )
 
-func Set(ctx context.Context, callback func(statusCmd *redis.StatusCmd), key string, value interface{}, expiration time.Duration) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func Set(ctx context.Context, key string, value interface{}, expiration time.Duration) {
 	redisCmd := &PipelineCmd{
-		Ctx:                ctx,
-		Operation:          operation_Set,
-		Key:                key,
-		Args:               []interface{}{value, expiration},
-		StatusCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_Set,
+		Key:       key,
+		Args:      []interface{}{value, expiration},
 	}
 	cmdListChannel <- redisCmd
 }
 
-func ZAdd(ctx context.Context, callback func(intCmd *redis.IntCmd), key string, members ...*redis.Z) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func ZAdd(ctx context.Context, key string, members ...*redis.Z) {
 	redisCmd := &PipelineCmd{
-		Ctx:             ctx,
-		Operation:       operation_ZAdd,
-		Key:             key,
-		Args:            []interface{}{},
-		IntCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_ZAdd,
+		Key:       key,
+		Args:      []interface{}{},
 	}
 	for _, v := range members {
 		redisCmd.Args = append(redisCmd.Args, v)
@@ -41,13 +43,14 @@ func ZAdd(ctx context.Context, callback func(intCmd *redis.IntCmd), key string, 
 	cmdListChannel <- redisCmd
 }
 
-func ZAddNX(ctx context.Context, callback func(intCmd *redis.IntCmd), key string, members ...*redis.Z) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func ZAddNX(ctx context.Context, key string, members ...*redis.Z) {
 	redisCmd := &PipelineCmd{
-		Ctx:             ctx,
-		Operation:       operation_ZAddNX,
-		Key:             key,
-		Args:            []interface{}{},
-		IntCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_ZAddNX,
+		Key:       key,
+		Args:      []interface{}{},
 	}
 	for _, v := range members {
 		redisCmd.Args = append(redisCmd.Args, v)
@@ -55,36 +58,39 @@ func ZAddNX(ctx context.Context, callback func(intCmd *redis.IntCmd), key string
 	cmdListChannel <- redisCmd
 }
 
-func HSet(ctx context.Context, callback func(intCmd *redis.IntCmd), key string, values ...interface{}) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func HSet(ctx context.Context, key string, values ...interface{}) {
 	redisCmd := &PipelineCmd{
-		Ctx:             ctx,
-		Operation:       operation_HSet,
-		Key:             key,
-		Args:            []interface{}{},
-		IntCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_HSet,
+		Key:       key,
+		Args:      []interface{}{},
 	}
 	redisCmd.Args = append(redisCmd.Args, values...)
 	cmdListChannel <- redisCmd
 }
 
-func Expire(ctx context.Context, callback func(boolCmd *redis.BoolCmd), key string, expiration time.Duration) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func Expire(ctx context.Context, key string, expiration time.Duration) {
 	redisCmd := &PipelineCmd{
-		Ctx:              ctx,
-		Operation:        operation_Expire,
-		Key:              key,
-		Args:             []interface{}{expiration},
-		BoolCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_Expire,
+		Key:       key,
+		Args:      []interface{}{expiration},
 	}
 	cmdListChannel <- redisCmd
 }
 
-func ZRemRangeByScore(ctx context.Context, callback func(intCmd *redis.IntCmd), key, min, max string) {
+//don't use pipeline for high-safety senario
+//as redis pipeline may have data lost problem
+func ZRemRangeByScore(ctx context.Context, key, min, max string) {
 	redisCmd := &PipelineCmd{
-		Ctx:             ctx,
-		Operation:       operation_ZRemRangeByScore,
-		Key:             key,
-		Args:            []interface{}{min, max},
-		IntCmd_callback: callback,
+		Ctx:       ctx,
+		Operation: operation_ZRemRangeByScore,
+		Key:       key,
+		Args:      []interface{}{min, max},
 	}
 	cmdListChannel <- redisCmd
 }
--- src/common/redis_pipeline/redis_pipeline.go
+++ src/common/redis_pipeline/redis_pipeline.go
@@ -7,13 +7,13 @@ import (
 	"github.com/coreservice-io/cli-template/basic"
 	"github.com/coreservice-io/cli-template/plugin/redis_plugin"
 	"github.com/coreservice-io/job"
-
 	"github.com/go-redis/redis/v8"
 )
 
 const exec_count_limit = 100
 const exec_interval_limit_millisec = 2500
 const exec_thread_count = 8
+const cmd_channel_limit = 20000
 
 var last_exec_time_unixmilli = time.Now().UTC().UnixMilli()
 
@@ -22,55 +22,44 @@ type PipelineCmd struct {
 	Operation string
 	Key       string
 	Args      []interface{}
-
-	StatusCmd_callback func(statusCmd *redis.StatusCmd)
-	IntCmd_callback    func(intCmd *redis.IntCmd)
-	BoolCmd_callback   func(boolCmd *redis.BoolCmd)
-
-	ResultCmd interface{}
 }
 
-var cmdListChannel = make(chan *PipelineCmd, 2000)
+var cmdListChannel = make(chan *PipelineCmd, cmd_channel_limit)
 
 func ScheduleRedisPipelineExec() {
 	const jobName = "ScheduleRedisPipelineExec"
 
-	job.Start(
-		//job process
-		jobName,
-		func() {
-			//limit goroutinue count
-			ch := make(chan bool, exec_thread_count)
-			for {
-				ch <- true
-				go func() {
+	for i := 0; i < exec_thread_count; i++ {
+		job.Start(
+			//job process
+			jobName,
+			func() {
+				for {
 					if len(cmdListChannel) < 100 && time.Now().UTC().UnixMilli()-last_exec_time_unixmilli < exec_interval_limit_millisec {
 						time.Sleep(250 * time.Millisecond)
 						//basic.Logger.Debugln("sleep")
-						<-ch
-						return
-					}
 
+						continue
+					}
 					exec()
-					<-ch
-				}()
-			}
-		},
-		//onPanic callback
-		func(panic_err interface{}) {
-			basic.Logger.Errorln(panic_err)
-		},
-		1,
-		// job type
-		// UJob.TYPE_PANIC_REDO  auto restart if panic
-		// UJob.TYPE_PANIC_RETURN  stop if panic
-		job.TYPE_PANIC_REDO,
-		// check continue callback, the job will stop running if return false
-		// the job will keep running if this callback is nil
-		nil,
-		// onFinish callback
-		nil,
-	)
+				}
+			},
+			//onPanic callback
+			func(panic_err interface{}) {
+				basic.Logger.Errorln(panic_err)
+			},
+			1,
+			// job type
+			// UJob.TYPE_PANIC_REDO  auto restart if panic
+			// UJob.TYPE_PANIC_RETURN  stop if panic
+			job.TYPE_PANIC_REDO,
+			// check continue callback, the job will stop running if return false
+			// the job will keep running if this callback is nil
+			nil,
+			// onFinish callback
+			nil,
+		)
+	}
 }
 
 func exec() {
@@ -88,7 +77,7 @@ outLoop:
 		case cmd := <-cmdListChannel:
 			switch cmd.Operation {
 			case operation_Set:
-				cmd.ResultCmd = pl.Set(cmd.Ctx, cmd.Key, cmd.Args[0], cmd.Args[1].(time.Duration))
+				pl.Set(cmd.Ctx, cmd.Key, cmd.Args[0], cmd.Args[1].(time.Duration))
 				cmds = append(cmds, cmd)
 
 			case operation_ZAdd:
@@ -96,7 +85,7 @@ outLoop:
 				for _, v := range cmd.Args {
 					z = append(z, v.(*redis.Z))
 				}
-				cmd.ResultCmd = pl.ZAdd(cmd.Ctx, cmd.Key, z...)
+				pl.ZAdd(cmd.Ctx, cmd.Key, z...)
 				cmds = append(cmds, cmd)
 
 			case operation_ZAddNX:
@@ -104,19 +93,19 @@ outLoop:
 				for _, v := range cmd.Args {
 					z = append(z, v.(*redis.Z))
 				}
-				cmd.ResultCmd = pl.ZAddNX(cmd.Ctx, cmd.Key, z...)
+				pl.ZAddNX(cmd.Ctx, cmd.Key, z...)
 				cmds = append(cmds, cmd)
 
 			case operation_HSet:
-				cmd.ResultCmd = pl.HSet(cmd.Ctx, cmd.Key, cmd.Args...)
+				pl.HSet(cmd.Ctx, cmd.Key, cmd.Args...)
 				cmds = append(cmds, cmd)
 
 			case operation_Expire:
-				cmd.ResultCmd = pl.Expire(cmd.Ctx, cmd.Key, cmd.Args[0].(time.Duration))
+				pl.Expire(cmd.Ctx, cmd.Key, cmd.Args[0].(time.Duration))
 				cmds = append(cmds, cmd)
 
 			case operation_ZRemRangeByScore:
-				cmd.ResultCmd = pl.ZRemRangeByScore(cmd.Ctx, cmd.Key, cmd.Args[0].(string), cmd.Args[1].(string))
+				pl.ZRemRangeByScore(cmd.Ctx, cmd.Key, cmd.Args[0].(string), cmd.Args[1].(string))
 				cmds = append(cmds, cmd)
 
 			default:
@@ -137,24 +126,4 @@ outLoop:
 		basic.Logger.Errorln("exec pipeline error:", err)
 		return
 	}
-	for _, cmd := range cmds {
-		switch cmd.Operation {
-		case operation_Set:
-			if cmd.StatusCmd_callback != nil {
-				cmd.StatusCmd_callback(cmd.ResultCmd.(*redis.StatusCmd))
-			}
-		case operation_ZAdd, operation_HSet, operation_ZRemRangeByScore:
-			if cmd.IntCmd_callback != nil {
-				cmd.IntCmd_callback(cmd.ResultCmd.(*redis.IntCmd))
-			}
-		case operation_Expire:
-			if cmd.BoolCmd_callback != nil {
-				cmd.BoolCmd_callback(cmd.ResultCmd.(*redis.BoolCmd))
-			}
-
-		default:
-
-		}
-	}
-
 }
