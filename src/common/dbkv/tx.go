d0c5a41c5a4360030d9360fcf35c84b650fda127
leo <leo@leos-MacBook-Pro.local>
2022-10-23 11:20:55 +0800

    update

--- src/common/dbkv/tx.go
+++ src/common/dbkv/tx.go
@@ -66,53 +66,24 @@ func DeleteDBKV_Id(tx *gorm.DB, id int64) error {
 
 func GetDBKV(tx *gorm.DB, id *int64, key *string, fromCache bool, updateCache bool) (*DBKVModel, error) {
 
-	//gen_key
-	ck := smart_cache.NewConnectKey("dbkv")
-	ck.C_Int64_Ptr("id", id).C_Str_Ptr("key", key)
+	var result *DBKVQueryResults
+	var err error
 
-	_key := redis_plugin.GetInstance().GenKey(ck.String())
-
-	/////
-	resultHolderAlloc := func() interface{} {
-		return &DBKVModel{}
-	}
-
-	/////
-	query := func(resultHolder interface{}) error {
-		queryResult := resultHolder.(*DBKVModel)
-
-		query := tx.Table("dbkv")
-		if id != nil {
-			query.Where("id = ?", *id)
-		}
-		if key != nil {
-			query.Where("dbkv.key =?", *key)
-		}
-
-		var total_count int64
-		query.Count(&total_count)
-
-		err := query.Find(queryResult).Error
-		if err != nil {
-			return err
-		}
-
-		if total_count == 0 {
-			return smart_cache.QueryNilErr
-		}
-
-		return nil
-	}
-
-	/////
-	sq_result, sq_err := smart_cache.SmartQuery(_key, resultHolderAlloc, fromCache, updateCache, 300, query, "GetDBKV")
-
-	/////
-	if sq_err != nil {
-		return nil, sq_err
+	if key == nil {
+		result, err = QueryDBKV(tx, id, nil, fromCache, updateCache)
 	} else {
-		return sq_result.(*DBKVModel), nil
+		result, err = QueryDBKV(tx, id, &[]string{*key}, fromCache, updateCache)
 	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if result.TotalCount == 0 {
+		return nil, smart_cache.QueryNilErr
+	}
+
+	return result.Kv[0], nil
 }
 
 type DBKVQueryResults struct {
